b96e2614a19bbb459aef7973896cb9e2fc66a8df
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-01-23 05:59:23 +0600

    Solved 26_remove-duplicates-from-sorted-array

---
+++ src/Array/26_remove-duplicates-from-sorted-array/Solution_A.js
@@ -0,0 +1,22 @@
+let nums = [5, 7, 7, 7, 9, 9, 9, 12];
+
+// While in for loop, Keep forwarding & just put the new unique item at the first of "nums"
+// While putting, put that new unique item after the last unique item that was set before
+
+// As the array is sorted, so to find a new unique item you can just check what was the last unique item you found.
+// If the last unique item is same of current item then it is not unique.
+
+function removeDuplicates(nums) {
+  let lastIndexWhereUniqueItemWasSet = 0;
+
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[lastIndexWhereUniqueItemWasSet] == nums[index]) {
+      // As both are same that means current ITEM is same as the previous ITEM which i set as UNIQUE item.
+    } else {
+      ++lastIndexWhereUniqueItemWasSet;
+      nums[lastIndexWhereUniqueItemWasSet] = nums[index];
+    }
+  }
+
+  return ++lastIndexWhereUniqueItemWasSet;
+}
