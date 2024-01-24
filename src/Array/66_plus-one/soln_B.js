4848657dae33f393e1cde429db987d37070cb070
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-01-24 09:45:59 +0600

    Solved 66 plus one problem

---
+++ src/Array/66_plus-one/soln_B.js
@@ -0,0 +1,16 @@
+const plusOne = function (digits = []) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    ++digits[i];
+    if (digits[i] > 9) {
+      digits[i] = 0;
+    } else {
+      return digits;
+    }
+  }
+
+  digits.unshift(1);
+
+  return digits;
+};
+
+plusOne([9]);
